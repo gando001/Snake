1732d295ffb89dd6121c0763d8e46b3805441bde
Kunal Madhav <gando_001@hotmail.com>
2014-03-31 12:32:19 +1300

    Initial commit of project along with initial game code

---
+++ Assets/Scripts/SnakeScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(5,5);
+
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate(){
+	
+		rigidbody2D.velocity = movement;
+	}
+}
